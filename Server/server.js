4f2a64cb9069835f205b7bbbec7b3144c68792ac
Abhijeet <abhijeetcode05@gmail.com>
2023-10-12 17:55:14 +0530

    connect to DB

--- Server/server.js
+++ Server/server.js
@@ -1,10 +1,18 @@
 import express from "express";
 const app = express();
+import dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
+const PORT = process.env.PORT || 4000;
+import connection from "./config/database.js";
+
+//MogoDB Connection
+connection();
 
 app.get("/", (req, res) => {
   res.send("Hello");
 });
 
-app.listen(4000, () => {
-  console.log("Listing");
+app.listen(PORT, () => {
+  console.log("Listing to", PORT);
 });
