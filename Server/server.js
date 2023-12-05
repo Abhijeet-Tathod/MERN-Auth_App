fec9d48cc73beb571322588db4ff21026e19c928
Abhijeet <abhijeetcode05@gmail.com>
2023-12-05 18:36:00 +0530

    cors

--- Server/server.js
+++ Server/server.js
@@ -10,7 +10,17 @@ import cors from "cors";
 //MogoDB Connection
 connection();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://auth-123.netlify.app",
+      "http://localhost:3000",
+      // Add more origins as needed
+    ],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true, // Enable credentials (if needed)
+  })
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
@@ -19,7 +29,6 @@ app.listen(PORT, () => {
   console.log("Listing to", PORT);
 });
 
-
 app.get("/", (req, res) => {
   res.send("Hello");
-})
+});
